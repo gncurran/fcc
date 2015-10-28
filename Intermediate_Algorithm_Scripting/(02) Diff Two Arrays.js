f56d2c01cdfbc0984d22f780ec6c2f35e86813c7
George Curran <george.n.curran@vanderbilt.edu>
2015-10-27 22:15:19 -0400

    Update (02) Diff Two Arrays.js

--- Intermediate_Algorithm_Scripting/(02) Diff Two Arrays.js
+++ Intermediate_Algorithm_Scripting/(02) Diff Two Arrays.js
@@ -1,14 +1,14 @@
 function diff(arr1, arr2) {
 
-var combined = arr1.concat(arr2);                                       // combine both arrays into one
-
-function check(elem) {
-  if (arr1.indexOf(elem) === -1 || arr2.indexOf(elem) === -1) {         // if an element is not found in either arr1 or arr2
-    return elem;                                                        // return that element
+  function check(elem) {                                                    // create a function to:
+    if (arr1.indexOf(elem) === -1 || arr2.indexOf(elem) === -1) {           // check whether an element is not found in arr1 or arr2
+      return elem;                                                          // if so, return that element
+      }
     }
-  }
+  
+  var combined = arr1.concat(arr2);                                         // combine both arrays into one
    
-  return combined.filter(check);                                        // returned new array with the elements that passed the check function 
+  return combined.filter(check);                                            // returned a new array populated with the elements that passed the check function we created
 }
 
 diff([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
